1294e20c31384d4eb6856dd749eaa4238943c0cd
iamnovaia <hy6kbrghgk@privaterelay.appleid.com>
2026-04-29 11:55:02 +0500

    Add files via upload

---
+++ photo js.js
@@ -0,0 +1,33 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/balabanov.JPG",
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/bodrov.JPG",
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/selyaniv.JPG",
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/astahov.JPG",
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/siemki.JPG",
+
+  "https://raw.githubusercontent.com/iamnovaia/brat/refs/heads/main/uspeh.JPG"
+
+];
+
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "LUNCH",
+
+  "A MASKED BALL",
+
+  "THE SUBWAY",
+
+  "GOVERNMENT BUREAU",
+
+  "THE WAITING ROOM",
+
+  "LANDSCAPE WITH FIGURES"
+
+];
